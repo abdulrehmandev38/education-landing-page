6e08d1551c74c703b240aa9d2056c2482b05510a
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-10-12 20:52:13 -0400

    add nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+const Nav = () => {
+  return (
+    <nav className="bg-primary fixed w-full z-10 py-4 px-8 flex justify-between items-center">
+      <img src="" alt="logo" />
+      <div className="flex justify-between gap-6 items-center">
+        <a
+          className="text-lg hover:text-secondary text-gray-500 font-bold"
+          href="#home"
+        >
+          Home
+        </a>
+        <a
+          className="text-lg hover:text-secondary text-gray-500 font-bold"
+          href="#about"
+        >
+          About
+        </a>
+        <a
+          className="text-lg hover:text-secondary text-gray-500 font-bold"
+          href="#services"
+        >
+          Services
+        </a>
+      </div>
+      <p className="text-white px-6 py-2 bg-secondary rounded-full font-bold">
+        Join
+      </p>
+    </nav>
+  );
+};
+export default Nav;
